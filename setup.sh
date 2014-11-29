7e552c2df703170ac5ad7da7ac1e137f9c429547
Jeymisson Oliveira <jeymissoncz@gmail.com>
2014-11-29 11:34:35 -0300

    Added setup script for ubuntu

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+#Install nodejs
+sudo apt-get update
+echo -e "Instaling nodejs..."
+sudo apt-get install nodejs
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo -e "Installing mongodb"
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+echo -e "Starting mongodb..."
+sudo service mongod start
+
+echo -e "Installing project 100_duvidas_server dependencies..."
+npm install
+
+echo -e "Done\n To begin run: npm start"
