0cca505e79bfdb8ad4c9f4ca903baccc4f9c9584
Lurian <luclurian@gmail.com>
2015-03-03 08:21:03 -0300

    Server side de adicao de recomendacao

---
+++ models/recommendationmodel.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Lurian on 01/03/2015.
+ */
+var mongoose = require('mongoose');
+var User = require('./usermodel');
+
+var RecommendationSchema = new mongoose.Schema(
+    {
+        description: {type: String},
+        teacher:{type:mongoose.Schema.Types.ObjectId, ref:"User"}
+    }
+);
+
+
+RecommendationSchema.methods.getUser = function(cb){
+    var recommendation = this;
+    User.find({"profile.recommendations":recommendation._id}, function(err, doc){
+        if(err){
+            cb(err, null);
+        }else{
+            cb(null, doc);
+        }
+    });
+};
+
+
+module.exports = mongoose.model("Recommendation", RecommendationSchema);
